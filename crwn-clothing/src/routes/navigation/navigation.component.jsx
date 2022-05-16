87040061164ada49800b9388639eb1fa4a6092cb
Orcun Zeybek <zeybekor-lokal@D-W-D824708.fhhnet.stadt.hamburg.de>
2022-05-16 20:28:14 +0200

    wir haben dropdown hinzugefügt und warenkorb und shop haben wir jetzt mit namen und bilder bestückt.

--- crwn-clothing/src/routes/navigation/navigation.component.jsx
+++ crwn-clothing/src/routes/navigation/navigation.component.jsx
@@ -1,7 +1,11 @@
 import { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 import {ReactComponent as CrwnLogo} from '../../assets/crown.svg';
+
 import { UserContext } from '../../contexts/user.context';
+
 import './navigation.styles.scss';
 import {signOutUser} from '../../utils/firebase/firebase.utils';
 
@@ -24,10 +28,10 @@ const Navigation = () => {
               : (<Link className="nav-link" to="/auth">
               SIGN IN
             </Link>
-
               )}
-          
+          <CartIcon />
         </div>
+        <CartDropdown />
       </div>
       <Outlet />
     </Fragment>
