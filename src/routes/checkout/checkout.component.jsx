4ee5a832ed5651894f8152605369a8b6649eb39a
arcin20 <orczey@gmail.com>
2022-06-06 20:18:19 +0200

    test

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -4,7 +4,7 @@ const Checkout = () => {
     return (
         <div>
         <h1>
-            Hier kommen Sie zum Warenkorb te 
+            Hier kommen Sie zum Warenkorb te asdasdads
         </h1>
         </div>
 
